52802f261be55258e52560b0ce289bf67f26d702
Guillermo Santos <santosguillermo360@outlook.com>
2026-03-27 03:20:12 +0000

    update to latest cosmos

--- src/ThreadingSample/GCWorker.cs
+++ src/ThreadingSample/GCWorker.cs
@@ -7,10 +7,21 @@ public static partial class GCWorker
 {
     public static void DoWork()
     {
+        int lastCollectedCount = 0;
+        int sleepMs = 5000;
+        
         while (true)
         {
-            Thread.Sleep(5000);
-            GarbageCollector.Collect();
+            Thread.Sleep(sleepMs);
+            var objectsCollected = GarbageCollector.Collect();
+            
+            // If many objects were collected, GC sooner; if few, GC later
+            if (objectsCollected > 1000)
+                sleepMs = Math.Max(1000, sleepMs - 500);  // More frequent
+            else if (objectsCollected < 100)
+                sleepMs = Math.Min(10000, sleepMs + 500); // Less frequent
+                
+            lastCollectedCount = objectsCollected;
         }
     }
 }
